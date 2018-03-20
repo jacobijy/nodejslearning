16495dc1aeaf2c1e45664d23d341a46a22087815
jacobijy <astony.jy@outlook.com>
2018-03-20 17:34:47 +0800

    Revert "Revert "mysql operation optimized""
    This reverts commit 911217778283213f493fcfc70d9652b0949e175a.

--- routes/input.js
+++ routes/input.js
@@ -2,37 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var config = require('../config');
+var check = require('../utils/check');
 
 var connection = mysql.createConnection(config.mysqldb);
-router.post('/', function(req, res, next) {
-  var params = {
-    name: req.body.name, 
-    url: req.body.url, 
-    alexa: req.body.alexa, 
-    country: req.body.country
-  };
-  inputwebsite(params);
-})
-
-var inputwebsite = function(params) {
-  var addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
-  if (!checkExist(params)) {
-    console.log('lack of infomation or already exists');
-    return;
-  }
-  var sqlParams = [params.name, params.url, params.alexa, params.country];
-  connection.query(addSql, sqlParams, function (err, result) {
-    if(err){
-     console.log('[INSERT ERROR] - ',err.message);
-     
-    }        
-
-   console.log('--------------------------INSERT----------------------------');
-   //console.log('INSERT ID:',result.insertId);        
-   console.log('INSERT ID:',result);        
-   console.log('-----------------------------------------------------------------\n\n');  
-  });
-}
 
 var checkAvailable = function(params) {
   console.log('checkavailable');
@@ -42,12 +14,16 @@ var checkAvailable = function(params) {
       console.log(key +' doesn\'t exist');
       return false;      
     }
+    if (key == 'url') {
+      if (!check.checkURL(attribute)) {
+        return false;
+      }
+    }
   }
   return true;
 }
 
 var checkExist = function(params) {
-  console.log('checkExist');
   if (!checkAvailable(params)) {
     console.log('checkavailable false');
     return false;
@@ -59,12 +35,47 @@ var checkExist = function(params) {
       console.log('[SELECT ERROR] - ', err.message);
       return;
     }
-         
+    
     if (result.length > 0) {
       console.log('already exists');
+      return false;
     }
-    return result.length==0;
+    else {
+      console.log('new input');
+      return true;
+    }
   });
 }
+
+var inputwebsite = function(params) {
+  var check_status = checkExist(params);
+  console.log(check_status);
+  if (!check_status) {
+    console.log('lack of information or already exists');
+    return;
+  }
+  var addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
+  var sqlParams = [params.name, params.url, params.alexa, params.country];
+  connection.query(addSql, sqlParams, function (err, result) {
+    if (err) {
+      console.log('[INSERT ERROR] - ', err.message);
+    }        
+
+    console.log('--------------------------INSERT----------------------------');
+    //console.log('INSERT ID:',result.insertId);        
+    console.log('INSERT ID:',result);        
+    console.log('-----------------------------------------------------------------\n\n');  
+  });
+}
+
+router.post('/', function(req, res, next) {
+  var params = {
+    name: req.body.name, 
+    url: req.body.url, 
+    alexa: req.body.alexa, 
+    country: req.body.country
+  };
+  inputwebsite(params);
+})
 
 module.exports = router;
