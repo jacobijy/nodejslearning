280568c48ae9952a1cc8fda50ad2845a0b12669b
jacobijy <astony.jy@outlook.com>
2018-03-23 00:27:00 +0800

    test socket.io

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,23 +5,33 @@ window.onload = function() {
   var field = document.getElementById("field");
   var sendButton = document.getElementById("send");
   var content = document.getElementById("content");
+  var name = this.document.getElementById('name');
 
   socket.on('message', function (data) {
       if(data.message) {
-          messages.push(data.message);
-          console.log(data.message);
+          messages.push(data);
+          console.log(data);
           var html = '';
           for(var i=0; i<messages.length; i++) {
-              html += messages[i] + '<br />';
+              html += ('<b>' + messages[i].username ? messages[i].username :'Server');
+              html += ':' + messages[i].message + '<br />';
           }
+          console.log(html);
           content.innerHTML = html;
+          content.scrollTop = content.scrollHeight;
       } else {
           console.log("There is a problem:", data);
       }
   });
 
   sendButton.onclick = function() {
-      var text = field.value;
-      socket.emit('send', { message: text });
+      if (name.value == '') {
+          alert('Enter Your Name');
+      }
+      else {
+        var text = field.value;
+        socket.emit('send', { message: text, username : name.value});
+        field.value = "";
+      }
   };
 }
