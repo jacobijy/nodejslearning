d3cf52a1d2de0c3a47aa504aee7f8c3545a80b6e
jacobijy <astony.jy@outlook.com>
2018-03-24 02:26:30 +0800

    add signup and new server

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,20 @@ var config = require('../config');
 var connection = mysql.createConnection(config.mysqldb);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.connect();
-  var query = 'SELECT * FROM websites';
-  connection.query(query, function(err, result) {
-    if(err) {
-      console.log('[SELECT ERROR] - ', err.message);
-      return;
-    }
-    res.render('index', { 
-      title: 'Express',
-      result: result
-    });
-  });
-  connection.end();
+  // connection.connect();
+  // var query = 'SELECT * FROM websites';
+  // connection.query(query, function(err, result) {
+  //   if(err) {
+  //     console.log('[SELECT ERROR] - ', err.message);
+  //     return;
+  //   }
+  //   res.render('index', { 
+  //     title: 'Express',
+  //     result: result
+  //   });
+  // });
+  // connection.end();
+  res.render('index', {title: 'Chat'})
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res, next) {
+  console.log(req.body);
+  res.redirect('../');
+});
+
+router.post('/login', function(req, res, next) {
+  res.redirect('../chat')
+});
+
 module.exports = router;
