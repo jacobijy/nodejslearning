73cedc64544fc007fb24a5fdf9099d6a95bc8a1b
jacobijy <astony.jy@outlook.com>
2018-04-15 03:32:18 +0800

    reconfig

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var config = {
 
     site_static_host: '', // 静态文件存储域名
     // 社区的域名
-    host: '192.168.137.19',
+    host: 'localhost',
     // host: '172.26.164.243',
     // host: 'localhost',
 
